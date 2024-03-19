c4b0fdb07c2100446bda58a06154aa88407fd6e8
AidasPa <aidas0330@gmail.com>
2024-03-19 15:50:49 +0200

    remove double quotes escape

---
+++ c_src/fxml.d
@@ -0,0 +1,5 @@
+c_src/fxml.o: c_src/fxml.c \
+ /home/aidas/.asdf/installs/erlang/24.3.4.14/erts-12.3.2.14/include/erl_nif.h \
+ /home/aidas/.asdf/installs/erlang/24.3.4.14/erts-12.3.2.14/include/erl_drv_nif.h \
+ /home/aidas/.asdf/installs/erlang/24.3.4.14/erts-12.3.2.14/include/erl_int_sizes_config.h \
+ /home/aidas/.asdf/installs/erlang/24.3.4.14/erts-12.3.2.14/include/erl_nif_api_funcs.h
